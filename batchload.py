6ed2d278760270e0b499efd129b8770180b7e375
Sylvester Yiadom <sylvesteryiadom@gmail.com>
2022-03-06 14:47:40 -0600

    updated python code

--- batchload.py
+++ batchload.py
@@ -9,6 +9,11 @@ content = "x" * 100
 for i in range(10):
     for j in range(10):
         print(i, j)
-        table.put_item(Item={'primk': i, 'seck': j, 'Content': {"S": content}})
+        table.put_item(
+            Item={
+                'pm_key': i,
+                'sc_key': j,
+                'Content': {
+                    "S": content}})
 
 print("Upload complete..")
